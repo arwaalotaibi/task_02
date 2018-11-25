93e896c6e29d45e35e84c40d5edfa2a0e30b411f
arwaalotaibi <arwa@arwas-MacBook-Pro.local>
2018-11-25 21:24:16 +0300

    update

--- restaurants/views.py
+++ restaurants/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
-# Create your views here.
+from django.http import HttpResponse
+
+def fun(request):
+    context = {
+        "msg": "Hello World!",
+    }
+    return render(request, 'file.html', context)
